b3c3d4a52e82721082b7ef99b8dcbf3f454e1bae
Leonardo Mota <contact@leoism.me>
2021-03-02 22:08:04 -0800

    Add UserState Class

---
+++ public_html/src/TurnSystemAPI/UserState.js
@@ -0,0 +1,24 @@
+/**
+ * Holds the state of a user.
+ */
+class UserState {
+  constructor() {
+    this.isActive = false;
+  }
+
+  /**
+   * Initializes the User with the data required
+   */
+  initialize() {}
+
+  /**
+   * Updates that the User should run. Example:
+   * Checking if the user can move or not (isActive).
+   */
+  update() {}
+
+  /**
+   * Action that a User can do. Example: Invoke an 'attack'
+   */
+  action() {}
+}
