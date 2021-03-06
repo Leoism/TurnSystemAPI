59959376356de4b9df98a4ac9514f84322c199f0
Leonardo Mota <contact@leoism.me>
2021-03-06 15:14:34 -0800

    Switch gamePick to false

--- public_html/src/Start/misc.js
+++ public_html/src/Start/misc.js
@@ -1,9 +1,8 @@
 window.onload = () => {
   let myGame;
-  let gamePick = true;
+  let gamePick = false;
   document.onkeydown = (event) => {
-    // TODO: Fix errors
-    if (event.key.toLowerCase() === 'p') {
+    if (event.key.toLowerCase() === '/') {
       myGame = gamePick ? new MainTurnDemo() : new AddRemovePlayerDemo();
       gEngine.Core.initializeEngineCore('GLCanvas', myGame);
       gamePick = !gamePick;
