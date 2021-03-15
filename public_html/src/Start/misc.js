5c49ed5fb32848eea76d9127346cd05b596d69eb
Chapman Tse <tch222000@hotmail.com>
2021-03-14 23:05:59 -0700

    Switch Scene misc.js

--- public_html/src/Start/misc.js
+++ public_html/src/Start/misc.js
@@ -2,16 +2,20 @@ window.onload = () => {
   let myGame;
   let gamePick = false;
   document.onkeydown = (event) => {
-    if (event.key.toLowerCase() === '1') {
+    if (event.key.toLowerCase() === '5') {
       myGame = new MainTurnDemo();
       gEngine.Core.initializeEngineCore('GLCanvas', myGame);
       gamePick = !gamePick;
     }
-    else if (event.key.toLowerCase() === '2') {
+    else if (event.key.toLowerCase() === '6') {
       myGame = new AddRemovePlayerDemo();
       gEngine.Core.initializeEngineCore('GLCanvas', myGame);
     }
-    else if (event.key.toLowerCase() === '3') {
+    else if (event.key.toLowerCase() === '7') {
+      myGame = new ConditionalDemo();
+      gEngine.Core.initializeEngineCore('GLCanvas', myGame);
+    }
+    else if (event.key.toLowerCase() === '8') {
       myGame = new ConditionalDemo();
       gEngine.Core.initializeEngineCore('GLCanvas', myGame);
     }
